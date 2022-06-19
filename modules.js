f08e3869c08f254e0e8a109f4ff56e81e672f0b5
Bhavya Phogat <b.phogat1925@gmail.com>
2022-06-19 10:45:10 -0400

    changes

--- modules.js
+++ modules.js
@@ -110,83 +110,83 @@ class Park {
 async function parseDataFile(parkData, getParkNamesOnly=false) {
     function parseData(text) {
         response = String(text).split('\n')
-            data = []
+        data = []
+        i=0
+        while (i < response.length) {
+            line = String(response[i]).replace(/(\r\n|\n|\r)/gm, "")
+            if (line != "") {
+                header = line.split(" ")[0]
+                contentIndex = header.length+1
+                content = line.substring(contentIndex).split(" - ")
+                data.push({header, content})
+            }
+            i++
+        }
+        
+        returnData = []
+        if (getParkNamesOnly) {
+            for (obj of data) {
+                if (obj.header == "START") {
+                    park = obj.content[0]
+                    if (!returnData.includes(park)) {
+                        returnData.push(park)
+                    }
+                }
+            }
+        } else {
             i=0
-            while (i < response.length) {
-                line = String(response[i]).replace(/(\r\n|\n|\r)/gm, "")
-                if (line != "") {
-                    header = line.split(" ")[0]
-                    contentIndex = header.length+1
-                    content = line.substring(contentIndex).split(" - ")
-                    data.push({header, content})
-                }
-                i++
-            }
-            
-            returnData = []
-            if (getParkNamesOnly) {
-                for (obj of data) {
-                    if (obj.header == "START") {
-                        park = obj.content[0]
-                        if (!returnData.includes(park)) {
-                            returnData.push(park)
-                        }
-                    }
-                }
-            } else {
-                i=0
-                while (i < data.length) {
-                    switch (data[i].header) {
-                        case "START":
-                            startData = data[i].content
+            while (i < data.length) {
+                switch (data[i].header) {
+                    case "START":
+                        startData = data[i].content
+                        returnData.push({
+                            header: "START",
+                            park: startData[0],
+                            trailType: startData[1],
+                            time: startData[2]
+                        })
+                        i+=1
+                        break
+                    case "TIME":
+                        if (data[i+1].header === "COORDS") {
+                            coords = data[i+1].content[0].split(',')
+                            coords = [parseFloat(coords[0]), parseFloat(coords[1])]
                             returnData.push({
-                                header: "START",
-                                park: startData[0],
-                                trailType: startData[1],
-                                time: startData[2]
+                                header: "COORDS",
+                                time: data[i].content[0],
+                                coords,
+                                accuracy: parseFloat(data[i+2].content[0])
                             })
-                            i+=1
-                            break
-                        case "TIME":
-                            if (data[i+1].header === "COORDS") {
-                                coords = data[i+1].content[0].split(',')
-                                coords = [parseFloat(coords[0]), parseFloat(coords[1])]
-                                returnData.push({
-                                    header: "COORDS",
-                                    time: data[i].content[0],
-                                    coords,
-                                    accuracy: parseFloat(data[i+2].content[0])
-                                })
-                                i+=3
-                            }
-                            else if (data[i+1].header === "POI") {
-                                coords = data[i+2].content[0].split(',')
-                                coords = [parseFloat(coords[0]), parseFloat(coords[1])]
-                                returnData.push({
-                                    header: "POI",
-                                    park: data[i+1].content[0],
-                                    description: data[i+1].content[1],
-                                    time: data[i].content[0],
-                                    coords,
-                                    accuracy: parseFloat(data[i+3].content[0]),
-                                    imgId: data[i+4].content[0],
-                                })
-                                i+=5
-                            }
-                            else {
-                                throw "error while parsing park data file"
-                            }
-                            break
-                        default:
+                            i+=3
+                        }
+                        else if (data[i+1].header === "POI") {
+                            coords = data[i+2].content[0].split(',')
+                            coords = [parseFloat(coords[0]), parseFloat(coords[1])]
+                            returnData.push({
+                                header: "POI",
+                                park: data[i+1].content[0],
+                                description: data[i+1].content[1],
+                                time: data[i].content[0],
+                                coords,
+                                accuracy: parseFloat(data[i+3].content[0]),
+                                imgId: data[i+4].content[0],
+                            })
+                            i+=5
+                        }
+                        else {
                             throw "error while parsing park data file"
-                    }
+                        }
+                        break
+                    default:
+                        throw "error while parsing park data file"
                 }
             }
+        }
 
-            return new Promise((resolve) => {
-                // console.log(returnData)
-                resolve(returnData)
-            })
+        return new Promise((resolve) => {
+            // console.log(returnData)
+            resolve(returnData)
+        })
     }
 
     if (parkData === null) {
